e6b6f7eac23d849a9feef90b467333ec825993fc
Capriskrald <hansenmann2497@hotmail.com>
2020-04-19 16:44:46 +0200

    Changed restaurant index to use asp, changed some styles to mobile css, and changed _layout to actually use mobile css when pixel below 480

--- Controllers/ResturantController.cs
+++ Controllers/ResturantController.cs
@@ -5,14 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using BreakfastWebAppV2.Data;
+using BreakfastWebAppV2.Models;
 
 namespace BreakfastWebAppV2.Controllers
 {
     public class RestaurantController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(RestaurantViewModel model)
         {
-            return View();
+            return View(model);
         }
     }
 }
---
+++ Models/RestaurantViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreakfastWebAppV2.Models
+{
+    public class RestaurantViewModel
+    {
+        public int roomNumber { get; set; }
+        public int childrenCount { get; set; }
+        public int parentsCount { get; set; }
+    }
+}
