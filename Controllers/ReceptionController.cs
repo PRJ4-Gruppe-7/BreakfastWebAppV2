996fb662a73220e003095abd63e8c07daca9d3f1
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-04-20 17:04:01 +0200

    Done with reception

--- Controllers/ReceptionController.cs
+++ Controllers/ReceptionController.cs
@@ -23,6 +23,11 @@ namespace BreakfastWebAppV2.Controllers
             return View(new ReceptionViewModel(rooms, new DateTime(2020, 4, 19)));
         }
 
+        public IActionResult Set()
+        {
+            return View(new ReceptionViewModel(null, new DateTime(2020, 4, 19)));
+        }
+
         [HttpPost]
         public ActionResult GetSubmit()
         {
@@ -31,17 +36,96 @@ namespace BreakfastWebAppV2.Controllers
         }
 
         [HttpPost]
-        public ActionResult SetSubmit()
+        public async Task<IActionResult> Edit(DateTime date, int RoomNumber, int AdultsWantBreakfast, int ChildrenWantBreakfast)
         {
-            string date = Request.Form["date"];
-            string roomnumber = Request.Form["RoomNumber"];
-            string adultsbreakfast = Request.Form["AdultsWantBreakfast"];
-            string childrenbreakfast = Request.Form["ChildrenWantBreakfast"];
 
-            //context.Add(new Occupants() { s})
 
-            return View("Get");
+            if (RoomNumber < 0)
+            {
+                return NotFound();
+            }
+
+            var room = await context.Rooms
+                .Where(r => r.RoomNumber == RoomNumber)
+                .Where(c => c.Occupants.Any(i => i.Date == date))
+                .Select(c => new
+                {
+                    c,
+                    Occupants = c.Occupants.Where(i => i.Date == date)
+                })
+                .FirstOrDefaultAsync();
+
+            if (room == null)
+            {
+                var room1 = await context.Rooms
+                .Where(r => r.RoomNumber == RoomNumber)
+                .Include(r => r.Occupants)
+                .FirstOrDefaultAsync();
+
+                room1.Occupants.Add(new Occupants
+                {
+                    Adult = AdultsWantBreakfast,
+                    Children = ChildrenWantBreakfast,
+                    Date = date
+                });
+
+            } 
+            else
+            {
+                room.c.Occupants.First().Adult = AdultsWantBreakfast;
+                room.c.Occupants.First().Children = ChildrenWantBreakfast;
+
+                
+            }
+
+            context.SaveChanges();
+
+
+            return View("Set", new ReceptionViewModel(null,date));
         }
+
+
+
+
+
+        //public ActionResult SetSubmit()
+        //{
+        //    string date_string = Request.Form["date"];
+        //    DateTime date = DateTime.Parse(date_string);
+        //    string roomnumber_string = Request.Form["RoomNumber"];
+        //    int roomnumber = int.Parse(roomnumber_string);
+        //    string adultsbreakfast_string = Request.Form["AdultsWantBreakfast"];
+        //    int adultsbreakfast = int.Parse(adultsbreakfast_string);
+        //    string childrenbreakfast_string = Request.Form["ChildrenWantBreakfast"];
+        //    int childrenbreakfast = int.Parse(childrenbreakfast_string);
+
+        //    //List<Room> RoomToEdit = context.Rooms
+        //    //    .Where(r => r.RoomNumber == roomnumber)
+        //    //    .Include(r => r.Occupants)
+        //    //    .ToList();
+
+
+        //     var RoomToEdit = context.Rooms
+        //        .Where(r => r.RoomNumber == roomnumber)
+        //        .Where(c => c.Occupants.Any(i => i.Date == date))
+        //        .Select(c => new
+        //        {
+        //            c,
+        //            Occupants = c.Occupants.Where(i => i.Date == date)
+        //        })
+        //        .FirstOrDefault();
+
+        //    //.Where(r => r.Occupants.Any(o => o.Date == date))
+
+        //    if (RoomToEdit != null)
+        //    {
+        //        //RoomToEdit.Occupants[0].;
+        //    }
+
+            
+
+        //    return View("Set");
+        //}
 
         public ReceptionController (ApplicationDbContext dbcont)
         {
