f24d0fad194639e80c97fb9103513cf0226f04e2
Rasmus Føgh Sørensen <au618865@uni.au.dk>
2020-04-20 18:12:16 +0200

    Finished Kitchen view

--- Controllers/KitchenController.cs
+++ Controllers/KitchenController.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BreakfastWebAppV2.Data;
 using BreakfastWebAppV2.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BreakfastWebAppV2.Controllers
 {
     public class KitchenController : Controller
     {
-        public IActionResult Index(KitchenViewModel model)
+        private ApplicationDbContext context;
+
+        public IActionResult Index()
         {
-            return View(model);
+            var rooms = context.Rooms.Include(r => r.Occupants).ToList();
+            return View("Index", new KitchenViewModel(rooms, new DateTime(2020, 4, 19)));
         }
 
-        [HttpGet]
+        [HttpPost]
         public ActionResult Submit()
         {
-            return View("Index", new KitchenViewModel(DateTime.Parse(Request.Form["date"])));
+            var rooms = context.Rooms.Include(r => r.Occupants).ToList();
+            return View("Index", new KitchenViewModel(rooms, DateTime.Parse(Request.Form["date"])));
+        }
+
+        public KitchenController(ApplicationDbContext dbcont)
+        {
+            context = dbcont;
         }
     }
 }
